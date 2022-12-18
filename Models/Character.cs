141a98ad34ec3dc4d3628df54092f07a957c48f0
Plusha99 <109800436+Plusha99@users.noreply.github.com>
2022-12-18 12:50:19 +0300

    Authentication

--- Models/Character.cs
+++ Models/Character.cs
@@ -15,6 +15,7 @@ namespace Dotnet7RpgGame.Models
         public int Agility { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
         public RpgClass Class { get; set; } = RpgClass.Knight;
+        public User? User { get; set; }
 
     }
 }
